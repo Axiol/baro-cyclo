a0fdf1c58288347aafa3e8f8fa3923866ad372eb
Arnaud Delante <contact@arnaud.ninja>
2021-03-28 19:50:06 +0200

    🏗 Allow the scrollwheel zoom

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -18,7 +18,7 @@ const Map: FC<MapProps> = ({position, boundaries}) => {
     const map = useMap();
     
     useEffect(() => {
-      map.setView(position, map.getZoom(), {
+      map.setView(position, 12, {
         animate: true,
       });
     });
@@ -29,7 +29,7 @@ const Map: FC<MapProps> = ({position, boundaries}) => {
   const purpleOptions = { color: 'purple' }
 
   return(
-    <StyledMapContainer center={position} zoom={12} scrollWheelZoom={false}>
+    <StyledMapContainer center={position} zoom={12} scrollWheelZoom={true} zoomControl={false}>
       <TileLayer
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         maxZoom={18}
